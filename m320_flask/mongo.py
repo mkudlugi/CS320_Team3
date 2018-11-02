ca794b2fed4bbcda75299b4273db61b5f879b227
Steviekong <sidharth.reji@gmail.com>
2018-11-02 10:11:25 -0400

    deleted old backend files and also made functions to return all json files and one random json file

--- m320_flask/mongo.py
+++ m320_flask/mongo.py
@@ -13,25 +13,20 @@ client = MongoClient('localhost', 27017)
 db = client.TEST_1
 collection = db['320IMPORT']
 
-@app.route('/all', methods=['GET'])
-def find_all():
+@app.route('/oneRand', methods=['GET'])
+def find_one_random():
   cursor = collection.find_one({})  
   page_sanitized = json.loads(json_util.dumps(cursor))
   return jsonify(page_sanitized)
 
-
-#app.config['MONGO_DBNAME'] = 'TEST_1'
-#app.config['MONGO_URI'] = 'mongodb://localhost:27017/TEST_1'
-
-#mongo = PyMongo(app)
-
-#@app.route('/star', methods=['GET'])
-#def get_all_users():
- # star = mongo.db.users.find({})
-  #output = []
-  #for s in star.find({}):
-   # output.append(s)
-  #return jsonify({'result' : output})
+@app.route('/all', methods=['GET'])
+def find_all():
+  cursor = collection.find({})
+  output = []
+  for s in cursor:
+    page_sanitized = json.loads(json_util.dumps(s))
+    output.append(page_sanitized)
+  return jsonify(output)
 
 if __name__ == '__main__':
     app.run()
