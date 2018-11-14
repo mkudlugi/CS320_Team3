981bb9cfff5d16f65c4d24f80f64ae2929441a46
Steviekong <sidharth.reji@gmail.com>
2018-11-13 22:34:04 -0500

    added tenants and updated error messages

--- m320_flask/mongo.py
+++ m320_flask/mongo.py
@@ -9,13 +9,13 @@ import json
 from bson import json_util, ObjectId
 
 application = Flask(__name__, template_folder = 'templates')
-client = MongoClient('localhost', 27017)
-db = client.TEST_1
-collection = db['320IMPORT']
+client = MongoClient('ec2-18-212-37-169.compute-1.amazonaws.com', 27017)
+db = client.handler
+collection = db['handler_json']
 
 @application.route('/', methods=['GET'])
 def find():
-  return jsonify("YO THIS SHIT WORKS")
+  return jsonify("Server up!")
 
 @application.route('/oneRand', methods=['GET'])
 def find_one_random():
@@ -28,7 +28,7 @@ def find_all():
   '''
   Fetches all of the 5000+ JSON files from mongo 
   '''
-  cursor = collection.find({})
+  cursor = collection.find({}).limit(10)
   if cursor.count() > 0:
     output = []
     for s in cursor:
@@ -38,11 +38,19 @@ def find_all():
     return "Database is empty", 404
   return jsonify(output)
 
-"""@application.route('/tenants/?', methods=['GET']):
+@application.route('/tenants', methods=['GET'])
 def tenants():
   query_params = request.args.get('tenants')
-  return jsonify([])
-"""
+  cursor = collection.find({'authorized.tenants':{'$elemMatch':{'$regex': query_params}}})
+  if cursor.count() > 0:
+    output = []
+    for s in cursor:
+      page_sanitized = json.loads(json_util.dumps(s))
+      output.append(page_sanitized)
+  else:
+    return "Error! No tenant was found!", 404
+  return jsonify(output)
+
 if __name__ == '__main__':
     application.debug = True
     application.run()
