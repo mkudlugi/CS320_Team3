c09cd2be956b88665dc57fc78c9fc6e1569d312f
Nick G <ngeorgian@umass.edu>
2018-12-10 17:38:45 -0500

    Completed search

--- front_end/src/Components/SearchBar.js
+++ front_end/src/Components/SearchBar.js
@@ -28,7 +28,7 @@ class SearchBar extends Component {
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
                     <input class="form-control mr-sm-2" id="searchInput" type="search" placeholder="Search" aria-label="Search"></input>
-                    <button class="btn my-2 my-sm-0" type="submit" onClick={this.props.onSearchClick}>Search</button>
+                    <button class="btn my-2 my-sm-0" type="submit" onClick={this.props.onSearchClick.bind(this)}>Search</button>
                 </form>
             </nav>
         )
